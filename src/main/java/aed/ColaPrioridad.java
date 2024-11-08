7a9a300dd48abc8b2909cefb0d8a8a62e6e4eb7c
0horane <lehoranes@proton.me>
2024-11-08 13:28:59 -0300

    ColaPrioridad refactor

--- src/main/java/aed/ColaPrioridad.java
+++ src/main/java/aed/ColaPrioridad.java
@@ -1,7 +1,6 @@
 package aed;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.GregorianCalendar;
 
 public class ColaPrioridad<T> {
     private ArrayList<Nodo<T>> datos;
@@ -23,6 +22,13 @@ public class ColaPrioridad<T> {
         heapify();
     }
     
+    public void agregar(Nodo<T> dato){ //O(log|datos|)
+        datos.add(dato);
+        int pos = datos.size()-1;
+        dato.pospropia = pos; 
+        siftUp(pos);
+    }
+
     public Nodo<T> maximo(){ //O(1)
         if (len() > 0){
             return (Nodo<T>) datos.get(0); 
@@ -31,30 +37,16 @@ public class ColaPrioridad<T> {
         }
     }
 
-    public void agregar(Nodo<T> dato){ //O(log|datos|)
-        datos.add(dato);
-        int pos = datos.size()-1;
-        dato.pospropia = pos; 
-        siftUp(pos);
-    }
-
     public Nodo<T> eliminar(int pos){
+        if (len() <= pos) return null;
+
         Nodo<T> res = datos.get(pos);
-        datos.set(pos, datos.get(datos.size()));
+        swap(res, datos.get(len()-1));
+        datos.remove(len()-1);
+
         siftUp(pos);
         siftDown(pos);
-        datos.remove(datos.size());
         return res;
-    }
-
-    public Nodo<T> sacarMaximo(){ // O(log |datos|)
-        if (len()==0) return null;
-        Nodo<T> dato = (Nodo<T>) datos.get(0);
-        Nodo<T> nuevoMax = (Nodo<T>) datos.get(len()-1);
-        swap(dato, nuevoMax);
-        datos.remove(len()-1);
-        siftDown(0);
-        return dato;
     }
     
     public int len(){
@@ -62,39 +54,50 @@ public class ColaPrioridad<T> {
     }
 
     private void siftUp(int index){ //O(log |datos|)
-        while (existeIndicePadre(index) && comparator.compare(datos.get(index), datos.get(indicePadre(index))) > 0){
-            int parentIndex  = indicePadre(index);
-            Nodo<T> datoactual = datos.get(index);
-            Nodo<T> datopadre = datos.get(parentIndex);
+        Nodo<T> dato = datos.get(index), padreD = padre(dato);
 
-            swap(datoactual, datopadre);
+        while (padreD != null && comparator.compare(dato, padreD) > 0){ 
+            swap(dato, padreD);
 
-            index = parentIndex;
+            dato = padreD;
+            padreD = padre(dato);
         }
     }
     
     private void siftDown(int index){ 
-        int greatestChildIndex;
-        while (existeIndiceHijoIzq(index)){
-            Nodo<T> datoactual = datos.get(index);
-            if (existeIndiceHijoDer(index) && comparator.compare(hijoIzq(datoactual), hijoDer(datoactual)) < 0){
-                greatestChildIndex = indiceHijoDer(index);
-            } else {
-                greatestChildIndex = indiceHijoIzq(index);
-            }
-
-            Nodo<T> greatestChild = datos.get(greatestChildIndex);
-            if (comparator.compare(datoactual, greatestChild) < 0 ){
-                swap(greatestChild, datoactual);
-                
-                index = greatestChildIndex;
-            } else {
-                break;
-            }
+        Nodo<T> dato = datos.get(index); 
+        Nodo<T> hijoD = hijoDer(dato);
+        Nodo<T> hijoI = hijoIzq(dato);
+        Nodo<T> mayor = mayorNodo(hijoD, hijoI);
+        
+        while (mayor != null && comparator.compare(dato, mayor) < 0){
+            swap(mayor, dato);
             
+            dato = mayor;
+            hijoD = hijoDer(dato);
+            hijoI = hijoIzq(dato);
+            mayor = mayorNodo(hijoI, hijoD);
         }
     }
 
+    private void swap(Nodo<T> nodo1, Nodo<T> nodo2){
+        datos.set(nodo1.pospropia, nodo2);
+        datos.set(nodo2.pospropia, nodo1);
+
+        int temp = nodo1.pospropia;
+        nodo1.pospropia = nodo2.pospropia;
+        nodo2.pospropia = temp;
+    }
+
+    private Nodo<T> mayorNodo(Nodo<T> nodo1, Nodo<T> nodo2){
+        if (nodo1 == null) return nodo2;
+        if (nodo2 == null) return nodo1;
+        if (comparator.compare(nodo1, nodo2) >= 0){
+            return nodo1;
+        } else {
+            return nodo2;
+        }
+    }
     
     private void heapify(){
         if (len() == 0) return;
@@ -108,66 +111,35 @@ public class ColaPrioridad<T> {
         }
     }
     
-    public String toString(){
-        return datos.toString();
-    }
-
-    private int indiceHijoIzq(int index){
-        return index*2+1;
-    }
-
-    private int indiceHijoDer(int index){
-        return index*2+2;
-    }
-
-    private int indicePadre(int index){
-        return (index-1)/2;
-    }
-
-    private Nodo<T> hijoIzq(Nodo<T> padre){
-        return datos.get(padre.pospropia*2+1);
+    private Nodo<T> hijoIzq(Nodo<T> padre){ 
+        if (padre.pospropia*2+1 >= len()){
+            return null;
+        } else {
+            return datos.get(padre.pospropia*2+1);
+        }
     }
 
     private Nodo<T> hijoDer(Nodo<T> padre){
-        return datos.get(padre.pospropia*2+2);
+        if (padre.pospropia*2+2 >= len()){
+            return null;
+        } else {
+            return datos.get(padre.pospropia*2+2);
+        }
     }
     private Nodo<T> padre(Nodo<T> hijo){
-        return datos.get((hijo.pospropia-1)/2);
+        if (hijo.pospropia == 0){
+            return null;
+        } else {
+            return datos.get((hijo.pospropia-1)/2);
+        }
     }
 
-    private Boolean existeHijoIzq(Nodo<T> padre){
-        return padre.pospropia*2+1 < len();
+    public String toString(){
+        ArrayList<T> arr = new ArrayList<>();
+        for (int i = 0; i<len();i++) {
+            arr.add(datos.get(i).valor);
+        }
+        return arr.toString();
     }
-
-    private Boolean existeHijoDer(Nodo<T> padre){
-        return padre.pospropia*2+2 < len();
-    }
-
-    private Boolean existePadre(Nodo<T> hijo){
-        return hijo.pospropia != 0;
-    }
-
-    private Boolean existeIndiceHijoIzq(int index){
-        return index*2+1 < len();
-    }
-
-    private Boolean existeIndiceHijoDer(int index){
-        return index*2+2 < len();
-    }
-
-    private Boolean existeIndicePadre(int index){
-        return index != 0;
-    }
-
-
-    private void swap(Nodo<T> nodo1, Nodo<T> nodo2){
-        datos.set(nodo1.pospropia, nodo2);
-        datos.set(nodo2.pospropia, nodo1);
-
-        int temp = nodo1.pospropia;
-        nodo1.pospropia = nodo2.pospropia;
-        nodo2.pospropia = temp;
-    }
-
 
 }
